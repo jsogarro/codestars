3f396588ccf7b35a5ad3cf65a915ed8d402f6051
Jamal O'Garro <jamal.ogarro@gmail.com>
2015-08-25 20:43:29 -0400

    adding sample app

---
+++ week_4/SimpleTableView/SimpleTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleTableView
+//
+//  Created by Jamal O'Garro on 8/24/15.
+//  Copyright (c) 2015 jso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ week_4/SimpleTableView/SimpleTableViewTests/SimpleTableViewTests.m
@@ -0,0 +1,40 @@
+//
+//  SimpleTableViewTests.m
+//  SimpleTableViewTests
+//
+//  Created by Jamal O'Garro on 8/24/15.
+//  Copyright (c) 2015 jso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SimpleTableViewTests : XCTestCase
+
+@end
+
+@implementation SimpleTableViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
